feea8ac8bba9b720109bdae8b82edf6afbf29f46
santivaarela <svarelavillarruel@itba.edu.ar>
2023-06-11 20:01:35 -0300

    js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+function buscar(){
+    var texto = document.getElementById('q').value;
+    if(texto == 'hyatt' || texto == 'marriot' || texto == 'plaza' || texto == 'conrad' || texto == 'mandarin' || texto == 'sherton'){
+        hotel = texto + '.html';
+        alert('Ud será redirigido a la página de ' + texto);
+        document.location.href=hotel;
+    }   else {
+        alert('No encontramos el hotel buscado.');
+    }
+} 
